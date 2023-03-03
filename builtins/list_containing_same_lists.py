ee316fdda2963f034065ba79d54d99c05afd6474
p1xckha <125886152+p1xckha@users.noreply.github.com>
2023-03-03 22:40:14 +0900

    Add files via upload

--- builtins/list_containing_same_lists.py
+++ builtins/list_containing_same_lists.py
@@ -3,48 +3,66 @@
 unexpected behaviors of lists
 """
 
-def are_all_elements_same(a:list[list[int]]) -> bool:
-    for i in range(1, len(a)):
-        if a[i-1] is not a[i]:
+def are_all_elements_same(list_of_lists: list[list[int]]) -> bool:
+    for i in range(1, len(list_of_lists)):
+        if list_of_lists[i-1] is not list_of_lists[i]:
             return False
     return True
 
-def print_message(a:list[list[int]]) -> None:
-    if are_all_elements_same(a):
-        print(a)
-        print("each list references to the same object")
-        ids_str = [id(a[i]) for i in range(len(a))]
-        print("IDs:", ids_str)
+def print_message(list_of_lists: list[list[int]]) -> None:
+    """
+    Prints a message indicating whether all the lists in the lists are the same object or not.
+    If they are the same object, also prints their IDs.
+    """
+    if are_all_elements_same(list_of_lists):
+        print("list_of_lists:", list_of_lists)
+        print("All lists reference the same object")
+        list_ids = [id(lst) for lst in list_of_lists]
+        print("List IDs:", list_ids)
+    else:
+        print("list_of_lists:", list_of_lists)
+        print("Lists do not all reference the same object")
+        list_ids = [id(lst) for lst in list_of_lists]
+        print("List IDs:", list_ids)
+    print("")
+
+    
+def print_message(list_of_lists:list[list[int]]) -> None:
+    if are_all_elements_same(list_of_lists):
+        print("list_of_lists:", list_of_lists)
+        print("All lists reference to the same object")
+        list_ids = [id(lst) for lst in list_of_lists]
+        print("List IDs:", list_ids)
         print("")
     else:
-        print(a)
-        print("each list does not references to the same object")
-        ids_str = [id(a[i]) for i in range(len(a))]
-        print("IDs:", ids_str)
+        print("list_of_lists:", list_of_lists)
+        print("Lists do not reference to the same object")
+        list_ids = [id(lst) for lst in list_of_lists]
+        print("List IDs:", list_ids)
+        print("")
     
-b = [2, 4, 6, 8]
+lst = [2, 4, 6, 8]
 
-# creates a new list with 3 references to the same list a.
-a = [b] * 3 # same as [b, b, b] 
-print_message(a)
-a[0][0] = -10
-print("a[0][0] = -10")
-print("a: ", a)
+# creates a new list  with 3 references to the same list b.
+list_of_lists = [lst] * 3 # same as [b, b, b] 
+print_message(list_of_lists)
+list_of_lists[0][0] = -10
+print("list_of_lists[0][0] = -10")
+print("list_of_lists: ", list_of_lists)
 print("")
 
 # another unexpected behavior of lists
-a = [b.copy()] * 3
-print_message(a)
+list_of_lists = [lst.copy()] * 3
+print_message(list_of_lists)
 
 # use list comprehension to avoid such unexpected behaviors of lists 
-print("a is created by list comprehension")
-a = [[0,0,0] for _ in range(3)] 
-print_message(a)
-print("")
+print("list_of_lists is created by list comprehension:")
+list_of_lists = [[0,0,0] for _ in range(3)] 
+print_message(list_of_lists)
 
-a[0][0] = 1
-print("a[0][0] = 1")
-print("a:", a) 
+list_of_lists[0][0] = 1
+print("list_of_lists[0][0] = 1")
+print("list_of_lists:", list_of_lists) 
 
 
 
