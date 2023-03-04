39561239ea7d0b3cbb1b5b8dbe55207dff87e99d
p1xckha <125886152+p1xckha@users.noreply.github.com>
2023-03-04 10:18:06 +0900

    Add files via upload

--- builtins/isinstance_example.py
+++ builtins/isinstance_example.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 
-def say_hello_to(name: str):
-    assert isinstance(name, str), "name must be string."
-    print("Hello " + name )
+def say_hello_to(friend: str):
+    assert isinstance(friend, str), "friend must be a string."
+    print("Hello " + friend )
 
-
-say_hello_to("John")
-
-try:
-    say_hello_to(1)
-except AssertionError as error:
-    print(error)
-
-
-
+if __name__ == "__main__":
+    say_hello_to("John")
+    
+    try:
+        say_hello_to(1)
+    except AssertionError as error:
+        print(error)
