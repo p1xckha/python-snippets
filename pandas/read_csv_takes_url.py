dc9b85fc058f4a28305179faba4f2f30144279d9
p1xckha <125886152+p1xckha@users.noreply.github.com>
2023-03-04 16:05:33 +0900

    Create read_csv_takes_url.py

---
+++ pandas/read_csv_takes_url.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+read_csv takes filepath as well as url.
+see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
+'''
+
+import pandas as pd
+
+filepath_as_url = "https://data.wa.gov/api/views/f6w7-q2d2/rows.csv?accessType=DOWNLOAD" # placeholder
+data = pd.read_csv(filepath_as_url)
+print(data.iloc[:10])
