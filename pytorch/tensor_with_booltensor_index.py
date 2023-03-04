a5b93fc3226f22c652fd3782d9e4aeb48848054f
p1xckha <125886152+p1xckha@users.noreply.github.com>
2023-03-04 13:37:40 +0900

    Create tensor_with_booltensor_index.py

---
+++ pytorch/tensor_with_booltensor_index.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+An index as boolean tensor for another integer tensor can be used 
+to divide the integer tensor into two class(0 and 1) 
+based on a threshold value.
+
+e.g. y[bool_tensor]
+"""
+
+import torch
+
+threshold_value = 0.2
+x = torch.arange(0,1,0.2)
+bool_tensor = x > threshold_value # tensor([True, True, False, ..., False])
+
+# x is divided into two classes (0 and 1) based on a threshold value of 0.2.
+y = torch.zeros(x.shape[0]) # initialize
+y[bool_tensor] = 1
+print("y:", y)
+
+# show the result of bool_tensor
+for i in range(len(x)):
+    print("x[%d]=%.1f > %.1f is %s" %(i, x[i], threshold_value, bool_tensor[i].item()) )
+print("")
+
+print("x:", x)
+print("bool_tensor:", bool_tensor)
+
+
+
