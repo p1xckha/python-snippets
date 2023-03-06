989eab877334b2a2db37b3a1ecc5232e81bf41d6
p1xckha <125886152+p1xckha@users.noreply.github.com>
2023-03-06 17:21:02 +0900

    Add files via upload

--- fastai/URLs_example.py
+++ fastai/URLs_example.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-
 '''
+print all urls in URLs
+
 fastai - External data
 https://docs.fast.ai/data.external.html
 '''
@@ -8,8 +9,9 @@ https://docs.fast.ai/data.external.html
 from fastai.vision.all import URLs
 import pprint 
 
+# vars(object) returns a mappingproxy object which is similar to a dict
 dict_of_urls = {}
-for key, value in vars(URLs).items(): # var(object) is dict
+for key, value in vars(URLs).items(): 
     if isinstance(value, str) and value.startswith("http"):
         dict_of_urls[key] = value
         
