3bc8c372d7fc2d2cb5f142555b6a5534b629e942
p1xckha <125886152+p1xckha@users.noreply.github.com>
2023-03-16 12:49:48 +0900

    Update lambda_matrix.py

--- builtins/lambda_matrix.py
+++ builtins/lambda_matrix.py
@@ -10,9 +10,12 @@ output: [[11, 12, 13], [21, 22, 23], [31, 32, 33], [41, 42, 43]]
 """
 
 matrix = lambda m, n: [[i*10+j for j in range(1, n+1)] for i in range(1, m+1)]
+three_d_arr = lambda m, n, l: [[[100*i+10*j+k for k in range(1,l+1)] for j in range(1, n+1)] for i in range(1, m+1)]
 
 a = matrix(4, 3)
 print("4 rows 3 columns matrix aij= 10*i + j as follow:\n", a)
+
+print(three_d_arr(3,4,5))
 
 
 # import numpy as np
