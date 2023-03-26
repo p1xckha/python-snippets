dc339745ed30c2ed2342037f94fd24f868370472
p1xckha <125886152+p1xckha@users.noreply.github.com>
2023-03-26 14:44:28 +0900

    Create conv2d_of_rgb_image.py

---
+++ pytorch/conv2d_of_rgb_image.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+import torchvision
+from PIL import Image
+
+# load the image and convert to RGB format to remove alpha channel
+image = Image.open('mario.png').convert('RGB')
+
+# convert the image to a tensor and add batch dimension
+image_tensor = transforms.ToTensor()(image).unsqueeze(0) # torch.Size([1, 3, 871, 481])
+
+# define the kernel weights for RGB images
+blur_kernel = torch.ones(3, 3, 3, dtype=torch.float32) / 9.0 # torch.Size([1, 1, 871, 481])
+
+# apply the convolution operation
+output = F.conv2d(image_tensor, blur_kernel.unsqueeze(0), padding=1)
+
+print(output.shape) # torch.Size([1, 1, 871, 481]) strange. not RGB image
+torchvision.utils.save_image(output , 'mario_conv.png') 
+
+
