5012eb79717e603ee980c8e496581849f5201df2
p1xckha <125886152+p1xckha@users.noreply.github.com>
2023-04-04 08:45:42 +0900

    Update np_apply_along_axis_example.py

--- numpy/np_apply_along_axis_example.py
+++ numpy/np_apply_along_axis_example.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-from typing import Union
+from typing import Sequence
 import numpy as np
 
 # Define a function that operates on 1D arrays
-def transform(x: Union[list, tuple, np.ndarray]):
+def transform(x: Sequence)-> np.array:
     assert len(x) == 2, "length of x must be 2"
     return np.array([x[0], x[0]*x[1], x[1]])
 
 # Create a 2D array
-arr = np.array([[0, 1], [2, 3], [4, 5]])
+x = np.array([[0, 1], [2, 3], [4, 5]])
 
 # Apply the transform function to each row of the array
-x_transformed = np.apply_along_axis(transform, axis=1, arr=arr)
+x_transformed = np.apply_along_axis(transform, axis=1, arr=x)
 print(x_transformed)  
-
