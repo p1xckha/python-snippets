afcebe57350db1d6bb7b61c9de6f82cbafa70eda
p1xckha <125886152+p1xckha@users.noreply.github.com>
2023-09-26 09:38:07 +0900

    Create winget.bat

---
+++ winget.bat
@@ -0,0 +1,38 @@
+:: https://learn.microsoft.com/ja-jp/windows/package-manager/winget/
+:: https://winget.run/
+
+winget install -e --id ProtonTechnologies.ProtonVPN
+winget install avast
+
+winget install --id=7zip.7zip  -e
+
+:: youtube-dl requirement
+winget install -e --id Microsoft.VCRedist.2010.x86
+
+winget install -e --id Anaconda.Anaconda3
+
+winget install -e --id Mozilla.Firefox
+winget install -e --id BraveSoftware.BraveBrowser
+
+winget install inkscape
+winget install -e --id VideoLAN.VLC
+
+winget install -e --id TheDocumentFoundation.LibreOffice
+winget install -e --id Adobe.Acrobat.Reader.64-bit
+
+winget install -e --id AOMEI.Backupper
+winget install -e --id KeePassXCTeam.KeePassXC
+winget install -e --id GnuPG.Gpg4win
+
+winget install -e --id GnuPG.Gpg4win
+winget install -e --id Dell.CommandUpdate.Universal
+
+
+winget install -e --id Microsoft.VisualStudio.2022.Community #install c++desktop
+winget install -e --id Git.Git
+
+winget install  MiKTeX.MiKTeX
+winget install TeXworks
+
+
+# https://raw.githubusercontent.com/p1xckha/python-snippets/main/requirements.txt
